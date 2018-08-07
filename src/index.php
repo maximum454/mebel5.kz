4befd490aaca17a044952327700dd858908b4c42
maxim <maximum454@gmail.com>
2018-08-07 10:31:07 +0600

    index

--- src/index.php
+++ src/index.php
@@ -1,8 +1,11 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?IncludeTemplateLangFile(__FILE__);?>
+<?$curPage = $APPLICATION->GetCurPage(true);?>
 <!doctype html>
-<html lang="ru">
+<html xml:lang="<?=LANGUAGE_ID?>" lang="<?=LANGUAGE_ID?>">
 <head>
     <meta charset="UTF-8">
-    
+
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
@@ -13,7 +16,7 @@
     <meta name="twitter:title" content="">
     <meta name="twitter:description" content="">
     <meta name="twitter:image" content="">
-    
+
     <!-- Open Graph data -->
     <meta property="og:title" content="">
     <meta property="og:type" content="article">
@@ -21,24 +24,26 @@
     <meta property="og:image" content="">
     <meta property="og:description" content="">
     <meta property="og:site_name" content="">
-    <title>КОНТАКТЫ</title>
+    <title><?$APPLICATION->ShowTitle()?></title>
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <link rel="stylesheet" href="css/mebel5.css">
+    <?$APPLICATION->ShowHead()?>
+    <?$APPLICATION->SetAdditionalCSS("/bitrix/templates/".SITE_TEMPLATE_ID."/css/mebel5.css");?>
 </head>
 <body>
+<?$APPLICATION->ShowPanel();?>
 <div class="wrp">
     <header class="header">
         <div class="container">
             <div class="row">
-                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12"><img class="header__logo" src="img/logo.png" alt=""></div>
+                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12"><img class="header__logo" src="<?=SITE_TEMPLATE_PATH?>/img/logo.png" alt=""></div>
                 <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
                     <div class="header__location">
-                        <img src="img/location.svg" alt="">Астана, ул. Жанажол, 4
+                        <img src="<?=SITE_TEMPLATE_PATH?>/img/location.svg" alt="">Астана, ул. Жанажол, 4
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-                    <a class="header__phone" href="tel:+77476760549"><img src="img/phone.svg" alt="">+7 747 676 05 49</a>
+                    <a class="header__phone" href="tel:+77476760549"><img src="<?=SITE_TEMPLATE_PATH?>/img/phone.svg" alt="">+7 747 676 05 49</a>
                 </div>
                 <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
                     <button class="header__btn popap_box" data-toggle="modal" data-target="#exampleModal">ЗАКАЗАТЬ ЗВОНОК</button>
@@ -50,7 +55,7 @@
     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>
                 <div class="modal-body">
                     <div class="row">
                         <div class="modal__header">Заказать обратный звонок</div>
@@ -75,66 +80,59 @@
             </div>
         </div>
     </div>
-    <nav class="navbar navbar-default" role="navigation">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                        data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-            </div>
-    
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav nav-justified">
-                    <li class="active"><a href="#">Главная</a></li>
-                    <li><a href="#">О нас</a></li>
-                    <li><a href="#">ВЫЗВАТЬ ЗАМЕРЩИКА</a></li>
-                    <li><a href="#">КАТАЛОГ</a></li>
-                    <li><a href="#">ОТЗЫВЫ</a></li>
-                    <li><a href="#">Контакты</a></li>
-                </ul>
-            </div>
-    
-        </div>
-    </nav>
+    <?$APPLICATION->IncludeComponent("bitrix:menu", "nav", Array(
+        "COMPONENT_TEMPLATE" => ".default",
+        "ROOT_MENU_TYPE" => "top",	// Тип меню для первого уровня
+        "MENU_CACHE_TYPE" => "N",	// Тип кеширования
+        "MENU_CACHE_TIME" => "3600",	// Время кеширования (сек.)
+        "MENU_CACHE_USE_GROUPS" => "Y",	// Учитывать права доступа
+        "MENU_CACHE_GET_VARS" => "",	// Значимые переменные запроса
+        "MAX_LEVEL" => "1",	// Уровень вложенности меню
+        "CHILD_MENU_TYPE" => "left",	// Тип меню для остальных уровней
+        "USE_EXT" => "N",	// Подключать файлы с именами вида .тип_меню.menu_ext.php
+        "DELAY" => "N",	// Откладывать выполнение шаблона меню
+        "ALLOW_MULTI_SELECT" => "N",	// Разрешить несколько активных пунктов одновременно
+    ),
+        false
+    );?>
+    <?$APPLICATION->IncludeComponent(
+        "bitrix:main.include",
+        "",
+        Array(
+            "AREA_FILE_SHOW" => "page",
+            "AREA_FILE_SUFFIX" => "slider",
+            "EDIT_TEMPLATE" => ""
+        )
+    );?>
     <main class="main">
-        <h1 class="main__header">КОНТАКТЫ</h1>
-        <div class="contacts">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-                        <div class="contacts__item location">
-                            Республика Казахстан, 010000
-                            г. Астана, ул. Жанажол, 4
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-                        <a class="contacts__item phone" href="tel:+77476760549">+7 747 676 05 49</a>
-                    </div>
-                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-                        <a class="contacts__item email" href="emailto:info@kovka5.kz">info@kovka5.kz</a>
-                    </div>
-        
-        
-                </div>
-                <div class="row">
-                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-                        <div class="contacts__item schedule">
-                            Режим работы:
-                            Пн - Сб с 09:00 до 20:00
-                        </div>
-                    </div>
-                </div>
-            </div>
-        
-        
-            <div class="contacts__map">
-                <img src="img/map.png" alt="">
+        <div class="container">
+            <div class="row">
+                <? if ($curPage != SITE_DIR."index.php") {?>
+                    <h1 class="main__header"><?=$APPLICATION->ShowTitle(false);?></h1>
+                <?} else {?>
+                    <h1 style="display: none"><?$APPLICATION->ShowTitle()?></h1>
+                <?} ?>
+                #WORK_AREA#
             </div>
         </div>
+        <?$APPLICATION->IncludeComponent(
+            "bitrix:main.include",
+            "",
+            Array(
+                "AREA_FILE_SHOW" => "page",
+                "AREA_FILE_SUFFIX" => "main-catalog",
+                "EDIT_TEMPLATE" => ""
+            )
+        );?>
+        <?$APPLICATION->IncludeComponent(
+            "bitrix:main.include",
+            "",
+            Array(
+                "AREA_FILE_SHOW" => "page",
+                "AREA_FILE_SUFFIX" => "now",
+                "EDIT_TEMPLATE" => ""
+            )
+        );?>
         <div class="feedback">
             <div class="container">
                 <div class="row">
@@ -156,64 +154,80 @@
                 </div>
             </div>
         </div>
+        <?$APPLICATION->IncludeComponent(
+            "bitrix:main.include",
+            "",
+            Array(
+                "AREA_FILE_SHOW" => "page",
+                "AREA_FILE_SUFFIX" => "testimonials",
+                "EDIT_TEMPLATE" => ""
+            )
+        );?>
+
+        <?$APPLICATION->IncludeComponent(
+            "bitrix:main.include",
+            "",
+            Array(
+                "AREA_FILE_SHOW" => "page",
+                "AREA_FILE_SUFFIX" => "banners",
+                "EDIT_TEMPLATE" => ""
+            )
+        );?>
     </main>
     <footer class="footer">
         <div class="container">
             <div class="row">
                 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
                     <h4>© BIG ELEPHANT GROUP</h4>
-                    <p class="footer__p">Наш банкетный зал находится в&nbsp;удобном месте в&nbsp;дали от&nbsp;суеты большого города, где вы&nbsp;сможете
-                        отметить свою торжественную дату или мероприятие не&nbsp;задумываясь ни&nbsp;о&nbsp;чем. Красивый
-                        интерьер и&nbsp;вкусная кухня не&nbsp;оставят ваших гостей равнодушными.</p>
+                    <p class="footer__p">Производство мебели одно из&nbsp;наших приоритетных направлений деятельности. Мы&nbsp;делаем качественную корпусную и&nbsp;индивидуальную мебель под все требования и&nbsp;пожелания заказчика. Только лучшие материалы и&nbsp;слаженная работа коллектива дают прекрасный результат и&nbsp;довольных клиентов.</p>
                 </div>
                 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
                     <h4>КОНТАКТЫ</h4>
-    
+
                     <p><b>Адрес:</b><br>
                         г. Астана, 010000, ул. Жанажол, 4</p>
-    
+
                     <p><b>Телефон:</b><br>
                         +7 747 676 05 49</p>
-    
+
                     <p><b>Электронная почта:</b><br>
                         <a href="mailto:bigelephant5@yandex.kz">bigelephant5@yandex.kz</a></p>
                 </div>
                 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
                     <h4>МЫ В СОЦИАЛЬНЫХ СЕТЯХ</h4>
-                    <a class="footer__social" href="#"><img src="img/fb.svg" alt=""></a>
-                    <a class="footer__social" href="#"><img src="img/inst.svg" alt=""></a>
-                    <a class="footer__social" href="#"><img src="img/vk.svg" alt=""></a>
-                    <a class="footer__social" href="#"><img src="img/youtube.svg" alt=""></a>
+                    <a class="footer__social" href="#"><img src="<?=SITE_TEMPLATE_PATH?>/img/fb.svg" alt=""></a>
+                    <a class="footer__social" href="#"><img src="<?=SITE_TEMPLATE_PATH?>/img/inst.svg" alt=""></a>
+                    <a class="footer__social" href="#"><img src="<?=SITE_TEMPLATE_PATH?>/img/vk.svg" alt=""></a>
+                    <a class="footer__social" href="#"><img src="<?=SITE_TEMPLATE_PATH?>/img/youtube.svg" alt=""></a>
                     <br>
                     <br>
                     <h4>МЫ ПРИНИМАЕМ К ОПЛАТЕ</h4>
                     <div class="accept-payment">
                         <div class="accept-payment__item">
-                            <img src="img/visa.svg" alt="">
+                            <img src="<?=SITE_TEMPLATE_PATH?>/img/visa.svg" alt="">
                         </div>
                         <div class="accept-payment__item">
-                            <img src="img/mastercard.svg" alt="">
+                            <img src="<?=SITE_TEMPLATE_PATH?>/img/mastercard.svg" alt="">
                         </div>
                         <div class="accept-payment__item">
-                            <img src="img/maestro.svg" alt="">
+                            <img src="<?=SITE_TEMPLATE_PATH?>/img/maestro.svg" alt="">
                         </div>
                         <div class="accept-payment__item">
-                            <img src="img/american-express.svg" alt="">
+                            <img src="<?=SITE_TEMPLATE_PATH?>/img/american-express.svg" alt="">
                         </div>
                         <div class="accept-payment__item">
-                            <img src="img/unionpay.svg" alt="">
+                            <img src="<?=SITE_TEMPLATE_PATH?>/img/unionpay.svg" alt="">
                         </div>
                         <div class="accept-payment__item">
-                            <img src="img/western-union.svg" alt="">
+                            <img src="<?=SITE_TEMPLATE_PATH?>/img/western-union.svg" alt="">
                         </div>
-    
+
                     </div>
                 </div>
             </div>
         </div>
     </footer>
 </div>
-
-<script src="js/mebel5.js"></script>
+<?$APPLICATION->AddHeadScript("/bitrix/templates/".SITE_TEMPLATE_ID."/js/mebel5.js");?>
 </body>
 </html>
---
+++ src/test.php
@@ -0,0 +1,35 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+<div class="banners">
+    <div class="container">
+        <div class="row">
+            <?foreach($arResult["ITEMS"] as $arItem):?>
+                <?
+                $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+                $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+                ?>
+                <div class="col-lg-6" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+                    <img
+                        src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
+                        width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
+                        height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
+                        alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
+                        title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
+                    />
+                </div>
+            <?endforeach;?>
+        </div>
+    </div>
+</div>
